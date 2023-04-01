34b5763ed2da8e65bb4c384e4578aa1058d5a1a3
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-04-01 18:34:57 +0200

    setup tabs

--- app/src/main/java/com/example/myhomebudgetkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/myhomebudgetkotlin/ui/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.myhomebudgetkotlin.ui
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.myhomebudgetkotlin.databinding.ActivityMainBinding
+import com.example.myhomebudgetkotlin.ui.adapters.FinanceAdapter
+import com.example.myhomebudgetkotlin.util.tabIcons
+import com.example.myhomebudgetkotlin.util.tabTitles
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +20,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        binding.viewPager.adapter = FinanceAdapter(this)
+
+        setupTabLayoutMediator(this, binding.tabLayout, binding.viewPager)
+    }
+
+    private fun setupTabLayoutMediator(
+        context: Context,
+        tabLayout: TabLayout,
+        viewPager2: ViewPager2) {
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            tab.apply {
+                icon = ContextCompat.getDrawable(context, tabIcons[position])
+                text = context.getString(tabTitles[position])
+                icon?.setTint(Color.WHITE)
+            }
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/example/myhomebudgetkotlin/util/TabsUtil.kt
@@ -0,0 +1,13 @@
+package com.example.myhomebudgetkotlin.util
+
+import com.example.myhomebudgetkotlin.R
+
+val tabTitles = listOf(
+    R.string.overview_name,
+    R.string.accounts_name,
+    R.string.bills_name)
+
+val tabIcons = listOf(
+    R.drawable.ic_overview,
+    R.drawable.ic_accounts,
+    R.drawable.ic_bills)
