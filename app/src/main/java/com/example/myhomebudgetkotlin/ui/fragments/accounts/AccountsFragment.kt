096cc9d79ae2b31a8fe0fd2eefed2e571b3f135f
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-04-02 00:26:48 +0200

    AccountsFragment.kt

--- app/src/main/java/com/example/myhomebudgetkotlin/ui/fragments/accounts/AccountsFragment.kt
+++ app/src/main/java/com/example/myhomebudgetkotlin/ui/fragments/accounts/AccountsFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import app.futured.donut.DonutSection
+import com.example.myhomebudgetkotlin.data.DataProvider
 import com.example.myhomebudgetkotlin.databinding.FragmentDetailInfoBinding
+import com.example.myhomebudgetkotlin.util.formatter
 
 class AccountsFragment : Fragment() {
 
@@ -18,11 +21,36 @@ class AccountsFragment : Fragment() {
     ): View {
         binding = FragmentDetailInfoBinding.inflate(inflater)
 
+        setupRecyclerView()
+        setupDonutChart()
+        setupTextViews()
+
+        return binding.root
+    }
+
+    private fun setupTextViews() {
+        binding.totalAmountTextView.text =
+            "${formatter.format(DataProvider.totalAccountsAmount)} zł"
+    }
+
+    private fun setupDonutChart() {
+        binding.donutViewAccount.apply {
+            val values = (0 until DataProvider.accounts.size).map {
+                DonutSection(
+                    DataProvider.accounts[it].name,
+                    DataProvider.accounts[it].color,
+                    DataProvider.accounts[it].amount.toFloat() /
+                            DataProvider.totalAccountsAmount.toFloat()
+                )
+            }
+            submitData(values)
+        }
+    }
+
+    private fun setupRecyclerView() {
         binding.recyclerView.apply {
             adapter = AccountsAdapter()
             layoutManager = LinearLayoutManager(this@AccountsFragment.requireContext())
         }
-
-        return binding.root
     }
 }
